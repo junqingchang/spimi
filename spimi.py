0c3f0bbf7b9e1ae8d91aebaf1924e8ba65d62d14
junqingchang <jqchang95@gmail.com>
2021-03-16 13:35:12 +0800

    Implement spimi invert fully (no merge)

--- spimi.py
+++ spimi.py
@@ -2,14 +2,21 @@ import os
 from nltk.stem import PorterStemmer
 import string
 import nltk
+import sys
+import time
+
+# Run if first time
+# nltk.download('stopwords')
 
 
 class SPIMI(object):
-    def __init__(self):
+    def __init__(self, output_dir):
         self.stemmer = PorterStemmer()
         self.exclusion = list(string.punctuation) + list(string.digits)
         self.stop_words = list(nltk.corpus.stopwords.words('english'))
-        self.posting_list = {}
+        self.output_dir = output_dir
+        if not os.path.exists(self.output_dir):
+            os.mkdir(self.output_dir)
 
     def directory_listing(self, root):
         files_in_dir = os.listdir(root)
@@ -17,7 +24,7 @@ class SPIMI(object):
         return files_in_dir
 
     def file_reading(self, filename):
-        with open(filename, 'r') as f:
+        with open(filename, 'r', encoding='utf8') as f:
             file_content = f.read()
         return file_content
 
@@ -39,25 +46,66 @@ class SPIMI(object):
         stem_tokens = [self.stemmer.stem(token) for token in alphabet_only_tokens]
         return stem_tokens
 
-    def create_posting_list(self, token, doc_id):
-        if token not in self.posting_list:
-            self.posting_list[token] = {doc_id: 1}
-        else:
-            if doc_id not in self.posting_list[token]:
-                self.posting_list[token][doc_id] = 1
-            else:
-                self.posting_list[token][doc_id] += 1
+    def add_to_dictionary(self, dictionary, term):
+        dictionary[term] = []
+        return dictionary[term]
 
-    def spimi_invert(self, root, block_size=500):
+    def get_postings_list(self, dictionary, term):
+        return dictionary[term]
+
+    def add_to_postings_list(self, postings_list, doc_id):
+        postings_list.append(doc_id)
+
+    def spimi_invert(self, root, block_size=100000):
         files_in_dir = self.directory_listing(root)
-        for idx, file in enumerate(files_in_dir):
-            file_content = self.file_reading(file)
+        block_num = 0
+        # 1 block is 512 bytes (tracked by sys.getsizeof)
+        max_byte = block_size * 512
+        current_size = 0
+        self.previous_dictionary = {}
+        self.dictionary = {}
+        self.start_time = time.time()
+        for doc_id in (files_in_dir):
+            file_content = self.file_reading(doc_id)
             tokens = self.linguistic_transform(self.tokenizer(file_content))
             for token in tokens:
-                self.create_posting_list(token, idx)
+                if token not in self.dictionary:
+                    current_size += sys.getsizeof(token)
+                    postings_list = self.add_to_dictionary(self.dictionary, token)
+                else:
+                    postings_list = self.get_postings_list(self.dictionary, token)
+                if current_size + sys.getsizeof(doc_id) > max_byte:
+                    self.write_block(self.dictionary, block_num)
+                    self.block_time(block_num, current_size/512)
+                    block_num += 1
+                    self.dictionary = {}
+                    current_size = 0
+                    current_size += sys.getsizeof(token)
+                    postings_list = self.add_to_dictionary(self.dictionary, token)
+                    current_size += sys.getsizeof(doc_id)
+                    self.add_to_postings_list(postings_list, doc_id)
+                else:
+                    current_size += sys.getsizeof(doc_id)
+                    self.add_to_postings_list(postings_list, doc_id)
+        if bool(self.dictionary):
+            self.write_block(self.dictionary, block_num)
+            self.block_time(block_num, current_size/512)
 
+    def block_time(self, block_num, block_size):
+        end_time = time.time()
+        print(f'Time take for BLOCK {block_num}: {end_time-self.start_time}, BLOCK {block_num} size: {block_size}')
+        self.start_time = time.time()
+
+    def write_block(self, dictionary, block_num):
+        sorted_terms = [term for term in sorted(dictionary)]
+        output_block_file = os.path.join(self.output_dir, f'BLOCK{block_num}.txt')
+        with open(output_block_file, 'w', encoding='utf8') as f:
+            for term in sorted_terms:
+                line = f'{term} {" ".join([str(doc_id) for doc_id in dictionary[term]])}'
+                f.write(line)
+        return output_block_file
             
 
 if __name__ == '__main__':
-    spimi = SPIMI()
+    spimi = SPIMI('output/')
     spimi.spimi_invert('HillaryEmails')
